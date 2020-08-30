b1efbba3fc93c7d572113a86a5bb9f460b850f52
Manoel Castro <manoelcastroadm@gmail.com>
2020-08-30 16:59:42 -0300

    Desafio Completo

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid } = require('uuid');
+const { v4: uuid } = require('uuid');
+const { isUuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +12,73 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
+
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0
+  }
+
+  repositories.push(repository);
+
+  return response.status(200).json(repository);
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+app.put("/repositories/:id", isId, (request, response) => {
+  const { id, repositoryIndex } = request.dados;
+  const { title, url, techs } = request.body;
+  const { likes } = repositories[repositoryIndex];
+  
+  const repository = {
+    id,
+    title,
+    url,
+    techs,
+    likes
+  }
+
+  repositories[repositoryIndex] = repository;
+
+  return response.status(200).json(repository);
 });
 
-app.delete("/repositories/:id", (request, response) => {
-  // TODO
+app.delete("/repositories/:id", isId, (request, response) => {
+  const { repositoryIndex } = request.dados;
+  repositories.splice(repositoryIndex, 1);
+  
+  return response.status(204).send();
+
 });
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+app.post("/repositories/:id/like", isId, (request, response) => {
+  const { repositoryIndex } = request.dados;
+  
+  const repository = repositories[repositoryIndex];
+
+  repository.likes++;
+  
+  return response.status(200).json(repository);
+
 });
+
+function isId(request, response, next) {
+  const { id } = request.params;
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+  
+  if(repositoryIndex < 0) {
+    return response.status(400).json({error: "Repository not found"});
+  }
+
+  request.dados = {id, repositoryIndex};
+
+  return next();
+}
 
 module.exports = app;
